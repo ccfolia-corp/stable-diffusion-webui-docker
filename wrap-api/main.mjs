ca0606dee6ae0abbed09a2dda08c04550b350b49
rassi0429 <taitokokoa+rassi@gmail.com>
2022-11-11 14:43:12 +0900

    feat: cors, more params

--- wrap-api/main.mjs
+++ wrap-api/main.mjs
@@ -1,7 +1,9 @@
 import axios from "axios"
 import express from "express"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use("/img", express.static('./img'));
 app.listen(3000, () => console.log("API OK at port 3000"))
 
@@ -10,11 +12,17 @@ if(!SD_ENDPOINT) throw new Error("Endpoint not provided")
 
 app.get("/generate",async (req, res) => {
     const prompt = req.query.prompt
+    let width = Number(req.query.width) || 512
+    if(width % 64 != 0) width = 512 
+    let height = Number(req.query.height) || 512
+    if(height % 64 != 0) height = 512 
+    let step = Number(req.query.steps) || 20
+    if(step >= 50) step = 50
     if(!prompt) {
         res.status(500).send("prompt not provided")
         return
     }
-    const {data} = await axios.post(SD_ENDPOINT + "/run/predict/", reqBody(prompt))
+    const {data} = await axios.post(SD_ENDPOINT + "/run/predict/", reqBody(prompt, width, height, step))
     const url = data.data[0][0].name
     if(!url) {
         res.status(500).send("Internal Server Error")
@@ -26,7 +34,7 @@ app.get("/generate",async (req, res) => {
 
 
 
-const reqBody = (data) => {
+const reqBody = (data, width, height, step) => {
     return {
         "fn_index": 50,
         "data": [
@@ -34,7 +42,7 @@ const reqBody = (data) => {
             "", // negative prompt
             "None",
             "None",
-            20,
+            step,
             "Euler a", // sampling method
             false,
             false,
@@ -47,8 +55,8 @@ const reqBody = (data) => {
             0,
             0,
             false, 
-            512, // width
-            512, // height
+            width, // width
+            height, // height
             false,
             0.7,
             0,
