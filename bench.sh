8a32078f493e8370cb8e76b94f7a13ca54bdddbb
rassi0429 <taitokokoa+rassi@gmail.com>
2022-11-11 17:25:50 +0900

    feat: bench並列化

--- bench.sh
+++ bench.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 URL="http://localhost:8887/generate?prompt=forest&width=768&height=512"
-N=10
-sum_time=0
 echo "URL: ${URL}"
-for i in $(seq ${N});do
-sleep 1
-start_time=`date +%s%3N`
-curl ${URL} -o /dev/null -s
-end_time=`date +%s%3N`
-run_time=$((end_time - start_time))
-sum_time=$((sum_time + run_time))
-echo "${i} end ${run_time}ms"
-done
-echo "all ${sum_time}ms"
+time (
+exec {o1}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o2}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o3}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o4}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o5}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o6}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o7}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o8}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o9}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+exec {o10}< <(curl ${URL} -o /dev/null -s -w "%{time_total}s\n")
+arr1[0]=$(cat <&${o1})
+arr1[1]=$(cat <&${o2})
+arr1[2]=$(cat <&${o3})
+arr1[3]=$(cat <&${o4})
+arr1[4]=$(cat <&${o5})
+arr1[5]=$(cat <&${o6})
+arr1[6]=$(cat <&${o7})
+arr1[7]=$(cat <&${o8})
+arr1[8]=$(cat <&${o9})
+arr1[9]=$(cat <&${o10})
+arr2=($(for v in "${arr1[@]}"; do echo "$v"; done|sort -n))
+for i in ${arr2[@]};do echo $i;done
+)
